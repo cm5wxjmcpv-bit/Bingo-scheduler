2e9af3282540492829cc198ecf3d339b9d4dc207
cm5wxjmcpv-bit <cm5wxjmcpv@privaterelay.appleid.com>
2026-03-23 12:38:21 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbxuAggSBbweXWID2eosdNORNmZpA4EFK3vb7kzAK37yiK5p49zBXEgWFGmQd-w0lrbm/exec',
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycby9Y2i8Z_FgWP0Q4c8Qxm3QzOuxuqp89UzBcDlFFRMiSWEm_C5L-sBReIvC27ib8KM/exec',
   requestTimeoutMs: 15000,
   storageKeys: {
     userSession: 'bingoUserSession',
